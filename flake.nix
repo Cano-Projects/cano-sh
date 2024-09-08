d4aa860f75dddae8dc9075075f3be59bedad9da6
Sigmanificient <edhyjox@gmail.com>
2024-09-09 00:38:19 +0200

    Add minimal flake

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+  outputs = { self, nixpkgs }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      hardeningDisable = ["format" "fortify"];
+      packages = with pkgs; [ gcc bear valgrind ncurses ];
+    };
+  };
+}
